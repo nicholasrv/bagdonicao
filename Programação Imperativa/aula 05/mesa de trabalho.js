c1c4dcced0de5111d9f51aecf9b2047adc1dcdea
nicholasrv <nicholasrv@live.com>
2021-10-26 23:09:01 -0300

    Atividades 26/10
    Atividades de front end carregadas para a main.

---
+++ Programação Imperativa/aula 05/mesa de trabalho.js
@@ -0,0 +1,8 @@
+////////////////////
+//Exercicio 1 - 
+function converterpolegadas(polegadas) {
+    return polegadas*2,54
+    
+   }
+   
+   console.log(converterpolegadas(10))
---
+++ Programação Imperativa/aula 05/mesadetrabalho.js
@@ -0,0 +1,8 @@
+////////////////////
+
+function converterpolegadas(polegadas) {
+    return polegadas*2,54
+    
+   }
+   
+   console.log(converterpolegadas(10))
