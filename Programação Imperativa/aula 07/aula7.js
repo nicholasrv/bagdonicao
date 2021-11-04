fbd47f21b3aa4c1a72d1979bf0b704b017f40162
nicholasrv <nicholasrv@live.com>
2021-11-03 21:52:10 -0300

    Create aula7.js
    OPERADORES LOGICOS

---
+++ Programação Imperativa/aula 07/aula7.js
@@ -0,0 +1,42 @@
+console.log(!true);
+
+console.log(!false);
+
+console.log(!!false);
+
+console.log(!!true);
+
+console.log(!1);
+
+console.log (!!1);
+
+console.log (!0);
+
+console.log(!!0);
+
+console.log(!!"");
+
+//let x = 5;
+//let y = 9;
+    //console.log((x!==5 && x < 10));
+    //console.log((x>9 || x=="5"));
+    //console.log((!(x===y)));
+        
+
+//let x = 10;
+//let y = "a";
+    //console.log((y==="b" || x >= 10));
+
+let x=3
+let y=8
+    console.log((!x == "3" || x === y) && !(y !== 8 && x <= y));
+
+let str = ""
+let msg = "haha!"
+let eBonito = "false"
+
+console.log(!str || msg) && eBonito);
+
+    
+    
+    
