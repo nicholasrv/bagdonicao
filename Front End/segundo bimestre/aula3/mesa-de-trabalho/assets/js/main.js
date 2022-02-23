b40bf8ac162e98d98dec9c845c370ca6532efc48
Felipel182 <nextage.info@gmail.com>
2022-02-23 20:39:32 -0300

    Commit Felipe Lopes

---
+++ Front End/segundo bimestre/aula3/mesa-de-trabalho/assets/js/main.js
@@ -0,0 +1,7 @@
+var terceiroItem1 = document.getElementById("linha-3_coluna-1");
+var terceiroItem2 = document.getElementById("linha-3_coluna-2");
+var terceiroItem3 = document.getElementById("linha-3_coluna-3");
+// E retornar o elemento DOM.
+terceiroItem1.innerText = "X";
+terceiroItem2.innerText = "X";
+terceiroItem3.innerText = "X";
