fe0ab2b93b4e34dfceb1820ecb9a8c010711797a
nicholasrv <nicholasrv@live.com>
2022-02-23 20:40:48 -0300

    COMMIT JOGO DA VELHA - LINHA 2, COLUNAS 1, 2 E 3 - NICHOLAS RUAS VIEGAS
    Arquivo modificado com o preenchimento da segunda linha na horizontal, com "x" - Nicholas Ruas Viegas.

---
+++ Front End/segundo bimestre/aula3/mesa-de-trabalho/assets/js/main.js
@@ -0,0 +1,7 @@
+var linha2coluna1 = document.getElementById("linha-2_coluna-1");
+var linha2coluna2 = document.getElementById("linha-2_coluna-2");
+var linha2coluna3 = document.getElementById("linha-2_coluna-3");
+
+linha2coluna1.innerText = "X"
+linha2coluna2.innerText = "X"
+linha2coluna3.innerText = "X"
